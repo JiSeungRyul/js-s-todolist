b178a99ef38156837043e69cacb693c5d37dd467
JISEUNGRYUL <jisr5736@gmail.com>
2024-08-13 10:27:12 +0900

    Modified vue changes to be reflected immediately.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.VUE_APP_BASE_URL || '/',
   transpileDependencies: true,
+  configureWebpack: {
+    watchOptions: {
+      poll: true,
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
